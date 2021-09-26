698366268fc0eefb1feb333d97922e20e6d51152
LvtLvt <44126697+LvtLvt@users.noreply.github.com>
2021-09-26 13:00:44 +0900

    Add: goroutine for aggregation

---
+++ data/aggregator.go
@@ -0,0 +1,164 @@
+package data
+
+import (
+	"fmt"
+	"github.com/djimenez/iconv-go"
+	"io"
+	"os"
+	"strings"
+	"sync"
+)
+
+const (
+	defaultFileFlag = os.O_RDWR | os.O_CREATE | os.O_APPEND
+	defaultFileMode = os.ModePerm
+)
+
+type Aggregator interface {
+	Aggregate()
+	NewReader(filePhase FilePhase) (io.Reader, error)
+}
+
+type FilePhase struct {
+	PrefixName string
+	CsvHead    []string
+}
+
+func (fp *FilePhase) GetCsvHeadStr() string {
+	return strings.Join(fp.CsvHead, "|") + "\n"
+}
+
+type FileAggregator struct {
+	Src          string
+	Dest         string
+	FilePhases   []FilePhase
+	FromEncoding string
+}
+
+// Aggregate whole files which belong to the src directory
+// example 주소_경기도.txt 주소_강원도.txt -----> 주소.txt
+//
+func (fa *FileAggregator) Aggregate() {
+	if len(fa.FromEncoding) > 0 {
+		fa.merge(makeEncodingFilter(fa.FromEncoding))
+	} else {
+		fa.merge()
+	}
+}
+
+func (fa *FileAggregator) NewReader(filePhase FilePhase) (io.Reader, error) {
+	return os.OpenFile(fa.Dest+"/"+filePhase.PrefixName+".txt", defaultFileFlag, defaultFileMode)
+}
+
+type workableType struct {
+	file      os.DirEntry
+	filePhase FilePhase
+}
+
+func (fa *FileAggregator) merge(filters ...func(bytes []byte) ([]byte, error)) {
+	path, _ := os.Getwd()
+	files, _ := os.ReadDir(path + "/" + fa.Src)
+
+	println(path)
+	println(len(files))
+
+	var wg sync.WaitGroup
+	var workableFiles []workableType
+
+	for _, file := range files {
+		var filePhase *FilePhase
+		if file.IsDir() {
+			continue
+		}
+		if filePhase = fa.matchFilePhase(file.Name()); filePhase == nil {
+			continue
+		}
+
+		workableFiles = append(workableFiles, workableType{file: file, filePhase: *filePhase})
+	}
+
+	wg.Add(len(workableFiles))
+
+	for _, workableFile := range workableFiles {
+		file := workableFile.file
+		filePhase := workableFile.filePhase
+		go func() {
+			rFile, _ := os.OpenFile(fa.Src+"/"+file.Name(), defaultFileFlag, defaultFileMode)
+
+			_ = os.Mkdir(fa.Dest, defaultFileMode)
+			wFile, _ := os.OpenFile(fa.Dest+"/"+filePhase.PrefixName+".txt", defaultFileFlag, defaultFileMode)
+
+			defer rFile.Close()
+			defer wFile.Close()
+			defer wg.Done()
+
+			var bytes = make([]byte, 1024*1000*1000)
+
+			// TODO: move to the place where before execution
+			fileInfo, _ := wFile.Stat()
+			if fileInfo.Size() == 0 {
+				wFile.WriteString(filePhase.GetCsvHeadStr())
+			}
+
+			for {
+				n, err := rFile.Read(bytes)
+
+				if err != nil {
+					if err == io.EOF || n == 0 {
+						break
+					} else {
+						_ = fmt.Errorf("while reading, error occured cause: %s", err.Error())
+						os.Exit(-1)
+					}
+				}
+
+				var out = bytes[:n]
+
+				// do filter
+				for _, filter := range filters {
+					out, _ = filter(out)
+				}
+
+				if err != nil {
+					_ = fmt.Errorf("while converting, error occured cause: %s", err.Error())
+					os.Exit(-1)
+				}
+
+				_, err = wFile.Write(out)
+
+				if err != nil {
+					_ = fmt.Errorf("while writing, error occured cause: %s", err.Error())
+					os.Exit(-1)
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+}
+
+func (fa *FileAggregator) matchFilePhase(filename string) *FilePhase {
+	for _, filePhase := range fa.FilePhases {
+		if strings.HasPrefix(filename, filePhase.PrefixName+"_") {
+			return &filePhase
+		}
+	}
+
+	return nil
+}
+
+func makeEncodingFilter(encoding string) func(bytes []byte) ([]byte, error) {
+	switch strings.ToUpper(encoding) {
+	case "EUC-KR":
+		return func(bytes []byte) ([]byte, error) {
+			str, err := iconv.ConvertString(string(bytes), encoding, "utf-8")
+			return []byte(str), err
+		}
+	default:
+		panic("unsupported encoding")
+	}
+}
+
+func appendCsvHead(names []string, writer io.Writer) {
+	writer.Write([]byte(strings.Join(names, "|")))
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module csvgo
 
 go 1.17
 
-require (
-	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da
-	github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8
-)
+require github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da
+
+require github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8 // indirect
